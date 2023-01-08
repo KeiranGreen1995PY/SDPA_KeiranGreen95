8f717f78ebe1e8b33838ea134fb7f6e48a349ba2
KeiranGreen1995PY <118452119+KeiranGreen1995PY@users.noreply.github.com>
2023-01-08 17:38:21 +0000

    Update Board.py
    Updated the board class for the dots and boxes game. created a board class with attributes such as rows, columns. Moreover, notes was also added to explain what each code and function of the class represents.

--- SDPA_Coursework/SDPA_Part1/Board.py
+++ SDPA_Coursework/SDPA_Part1/Board.py
@@ -7,10 +7,30 @@ Created on Sat Jan  7 15:05:40 2023
 
 ### Part 1 SDPA Coursework (Dots & Boxes Game) Board Class
 
-### This python file Board.py contains the board class for the text-based dots and boxes game
+# This python file Board.py contains the board class for the text-based dots
+# and boxes game
+# This script creates a board class with a grid of dots and methods for 
+# drawing the lines between the dots and checking to see if a player has
+# completed a box.
 
-class Board:
-    def __init__(self, size):
-       self.size = size
-       self.grid = [[" " for i in range(size)] for j in range(size)]
+class Board: #Create a class
+    def __init__(self, rows, cols): #Intiate the attributes for the class Board
+       self.rows = rows # Intiate the attribute row for the class Board
+       self.cols = cols # Intiate the attribute cols fo the class Board
+       self.grid = [[0 for _ in range(cols)] for _ in range(rows)]
+       
+       def Draw_Line(self, row, col, horizontal):
+           if horizontal:
+               self.grid[row][col] +=1
+           else:
+               self.grid[row][col] =+2
+               
+    def Check_Box(self, row, col):
+           if  self.grid[col][row] ==3:
+                return True
+           return False
+       
+    def Draw_Board(self):
+        for row in self.grid: 
+            print(" ".join(str(x) for x in row))
     
